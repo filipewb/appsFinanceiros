392c41d56b51faae61a0ad3ce62cf429f3dcf7bc
Filipe Wolf Boeck <85641580+filipewb@users.noreply.github.com>
2023-11-20 10:39:00 -0300

    criacao da tela de signup

---
+++ financy_app/lib/features/sign_up/sign_up_page.dart
@@ -0,0 +1,68 @@
+import 'dart:developer';
+
+import 'package:flutter/material.dart';
+
+import '../../common/constants/app_colors.dart';
+import '../../common/constants/app_text_styles.dart';
+import '../../common/widgets/multi_text_button.dart';
+import '../../common/widgets/primary_button.dart';
+
+class SignUpPage extends StatelessWidget {
+  const SignUpPage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: ListView(
+        children: [
+          Text(
+            'Spend Smarter',
+            textAlign: TextAlign.center,
+            style: AppTextStyles.mediumText.copyWith(
+              color: AppColors.greenLightTwo,
+            ),
+          ),
+          Text(
+            'Save More',
+            textAlign: TextAlign.center,
+            style: AppTextStyles.mediumText.copyWith(
+              color: AppColors.greenLightTwo,
+            ),
+          ),
+          Image.asset('assets/images/sign_up_image.png'),
+          TextFormField(),
+          const TextField(),
+          Padding(
+            padding: const EdgeInsets.only(
+              left: 32,
+              right: 32,
+              top: 16,
+              bottom: 4,
+            ),
+            child: PrimaryButton(
+              text: 'Sign Up',
+              onPressed: () {},
+            ),
+          ),
+          MultiTextButton(
+            onPressed: () => log('message'),
+            children: [
+              Text(
+                'Already have account? ',
+                style: AppTextStyles.smallText.copyWith(
+                  color: AppColors.grey,
+                ),
+              ),
+              Text(
+                'Log In',
+                style: AppTextStyles.smallText.copyWith(
+                  color: AppColors.greenLightTwo,
+                ),
+              ),
+            ],
+          ),
+        ],
+      ),
+    );
+  }
+}
