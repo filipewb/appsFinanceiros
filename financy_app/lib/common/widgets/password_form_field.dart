795ca408be7c77ea7c7582d0cd17935e0dda8947
Filipe Wolf Boeck <85641580+filipewb@users.noreply.github.com>
2023-11-21 13:44:32 -0300

    criacao do form field para senha

---
+++ financy_app/lib/common/widgets/password_form_field.dart
@@ -0,0 +1,49 @@
+// ignore_for_file: public_member_api_docs, sort_constructors_first
+import 'dart:developer';
+
+import 'package:flutter/material.dart';
+
+import 'package:financy_app/common/widgets/custom_text_form_field.dart';
+
+class PasswordFormField extends StatefulWidget {
+  final TextEditingController? controller;
+  final EdgeInsetsGeometry? padding;
+  final String? hintText;
+  final String? labelText;
+
+  const PasswordFormField({
+    Key? key,
+    this.controller,
+    this.padding,
+    this.hintText,
+    this.labelText,
+  }) : super(key: key);
+
+  @override
+  State<PasswordFormField> createState() => _PasswordFormFieldState();
+}
+
+class _PasswordFormFieldState extends State<PasswordFormField> {
+  bool isHidden = true;
+
+  @override
+  Widget build(BuildContext context) {
+    return CustomTextFormField(
+      obscureText: isHidden,
+      controller: widget.controller,
+      padding: widget.padding,
+      hintText: widget.hintText,
+      labelText: widget.labelText,
+      suffixIcon: InkWell(
+        borderRadius: BorderRadius.circular(23),
+        child: Icon(isHidden ? Icons.visibility : Icons.visibility_off),
+        onTap: () {
+          log("pressionou o olho");
+          setState(() {
+            isHidden = !isHidden;
+          });
+        },
+      ),
+    );
+  }
+}
