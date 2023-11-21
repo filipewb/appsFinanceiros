2dd2ddf89162c9cb2d39aa6ca09ab1017a2b3daf
Filipe Wolf Boeck <85641580+filipewb@users.noreply.github.com>
2023-11-21 14:16:48 -0300

    update

--- financy_app/lib/features/sign_up/sign_up_page.dart
+++ financy_app/lib/features/sign_up/sign_up_page.dart
@@ -1,6 +1,7 @@
 // ignore_for_file: public_member_api_docs, sort_constructors_first
 import 'dart:developer';
 
+import 'package:financy_app/common/utils/uppercase_text_formatter.dart';
 import 'package:financy_app/common/widgets/password_form_field.dart';
 import 'package:flutter/material.dart';
 
@@ -10,8 +11,15 @@ import '../../common/widgets/custom_text_form_field.dart';
 import '../../common/widgets/multi_text_button.dart';
 import '../../common/widgets/primary_button.dart';
 
-class SignUpPage extends StatelessWidget {
+class SignUpPage extends StatefulWidget {
   const SignUpPage({super.key});
+
+  @override
+  State<SignUpPage> createState() => _SignUpPageState();
+}
+
+class _SignUpPageState extends State<SignUpPage> {
+  final _formKey = GlobalKey<FormState>();
 
   @override
   Widget build(BuildContext context) {
@@ -33,20 +41,44 @@ class SignUpPage extends StatelessWidget {
             ),
           ),
           Image.asset('assets/images/sign_up_image.png'),
-          const Form(
+          Form(
+            key: _formKey,
             child: Column(
               children: [
                 CustomTextFormField(
                   labelText: "your name",
-                  hintText: "John Doe",
+                  hintText: "JOHN DOE",
+                  inputFormatters: [
+                    UpperCaseTextInputFormatter(),
+                  ],
+                  validator: (value) {
+                    if (value != null && value.isEmpty) {
+                      return "esse campo aqui nao";
+                    }
+                    return null;
+                  },
                 ),
                 PasswordFormField(
                   labelText: "choose your password",
                   hintText: "*********",
+                  validator: (value) {
+                    if (value != null && value.isEmpty) {
+                      return "esse campo aqui nao";
+                    }
+                    return null;
+                  },
+                  helperText:
+                      "Must have at least 8 characters, 1 capital letter and 1 number.",
                 ),
                 PasswordFormField(
                   labelText: "confirm your password",
                   hintText: "*********",
+                  validator: (value) {
+                    if (value != null && value.isEmpty) {
+                      return "esse campo aqui nao";
+                    }
+                    return null;
+                  },
                 ),
               ],
             ),
@@ -60,7 +92,13 @@ class SignUpPage extends StatelessWidget {
             ),
             child: PrimaryButton(
               text: 'Sign Up',
-              onPressed: () {},
+              onPressed: () {
+                final valid = _formKey.currentState != null &&
+                    _formKey.currentState!.validate();
+                if (valid) {
+                  log("");
+                } else {}
+              },
             ),
           ),
           MultiTextButton(
