e7c1d9eacd32dcb2a02b0ca81833491c01ea799f
Filipe Wolf Boeck <85641580+filipewb@users.noreply.github.com>
2023-12-12 09:12:56 -0300

    criacao do estado

---
+++ financy_app/lib/features/sign_up/sign_up_state.dart
@@ -0,0 +1,9 @@
+abstract class SignUpState {}
+
+class SignUpLoadingState extends SignUpState {}
+
+class SignUpInitialState extends SignUpState {}
+
+class SignUpSuccessState extends SignUpState {}
+
+class SignUpErrorState extends SignUpState {}
