076d9719806ff2caf2317e1e08b79449c5e12b3c
Filipe Wolf Boeck <85641580+filipewb@users.noreply.github.com>
2023-12-13 08:33:36 -0300

    update

--- financy_app/lib/features/sign_up/sign_up_controller.dart
+++ financy_app/lib/features/sign_up/sign_up_controller.dart
@@ -1,9 +1,12 @@
-import 'dart:developer';
-
 import 'package:financy_app/features/sign_up/sign_up_state.dart';
+import 'package:financy_app/services/auth_service.dart';
 import 'package:flutter/foundation.dart';
 
 class SignUpController extends ChangeNotifier {
+  final AuthService _service;
+
+  SignUpController(this._service);
+
   SignUpState _state = SignUpInitialState();
 
   SignUpState get state => _state;
@@ -13,19 +16,23 @@ class SignUpController extends ChangeNotifier {
     notifyListeners();
   }
 
-  Future<bool> doSignUp() async {
+  Future<void> signUp({
+    required String name,
+    required String email,
+    required String password,
+  }) async {
     _changeState(SignUpLoadingState());
 
     try {
-      await Future.delayed(const Duration(seconds: 2));
-
-      log("usuario logado");
+      await _service.signUp(
+        name: name,
+        email: email,
+        password: password,
+      );
 
       _changeState(SignUpSuccessState());
-      return true;
     } catch (e) {
-      _changeState(SignUpErrorState());
-      return false;
+      _changeState(SignUpErrorState(e.toString()));
     }
   }
 }
