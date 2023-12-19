d55dc91835d4a6e9df362876fb35ad9a68e174ae
Filipe Wolf Boeck <85641580+filipewb@users.noreply.github.com>
2023-12-19 17:29:52 -0300

    update

--- financy_app/lib/app.dart
+++ financy_app/lib/app.dart
@@ -1,6 +1,7 @@
 // import 'package:financy_app/common/themes/default_theme.dart';
 import 'package:financy_app/common/constants/routes.dart';
 import 'package:financy_app/features/onboarding/onboarding_page.dart';
+import 'package:financy_app/features/sign_in/sign_in_page.dart';
 import 'package:financy_app/features/sign_up/sign_up_page.dart';
 import 'package:financy_app/features/splash/splash_page.dart';
 import 'package:flutter/material.dart';
@@ -17,6 +18,7 @@ class App extends StatelessWidget {
         NamedRoutes.initial:(context) => const OnboardingPage(),
         NamedRoutes.splash:(context) => const SplashPage(),
         NamedRoutes.signUp:(context) => const SignUpPage(),
+        NamedRoutes.signIn:(context) => const SignInPage(),
       },
     );
   }
